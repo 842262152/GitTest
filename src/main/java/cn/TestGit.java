ea4c554385a30b260759ee2af72a9851998869c4
jct <842262152@qq.com>
2020-06-05 09:36:18 +0800

    从GitHub中拿到的最新版本

--- src/main/java/cn/TestGit.java
+++ src/main/java/cn/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("最开始的版本");
 		System.out.println("我添加了方法你要先更新才能用");
+		System.out.println("从GitHub拿到的版本");
 	}
 }
